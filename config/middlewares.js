f4620bfe406394a6d097307cfc167c90f3aa0ef4
unknown <aneeshasreeraj@gmail.com>
2025-07-19 16:09:39 +0530

    updated vercel url

--- config/middlewares.js
+++ config/middlewares.js
@@ -3,7 +3,7 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:3000', 'http://localhost:3001','https://ecom-9qq0k3l0a-aneesha-kps-projects.vercel.app/'],
+      origin: ['http://localhost:3000', 'http://localhost:3001','https://aneeshashop-page-eaziqxcxl-aneesha-kps-projects.vercel.app'],
       credentials: true,
     },
   },
